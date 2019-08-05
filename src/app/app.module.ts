70274969a3a5d4bde9ac73dcde7f4c8dfb789ec8
bhandhariya <sainiraja59@gmail.com>
2019-08-05 18:09:18 +0530

    5-aug last commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { IndexComponent } from './index/index.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
+import { Slider1Component } from './shared/slider1/slider1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { AboutUsComponent } from './shared/about-us/about-us.component';
+import { FeatureComponent } from './shared/feature/feature.component';
+import { WhyChooseUsComponent } from './shared/why-choose-us/why-choose-us.component';
 
 
 
@@ -14,7 +19,12 @@ import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    Slider1Component,
+    HeaderComponent,
+    AboutUsComponent,
+    FeatureComponent,
+    WhyChooseUsComponent
   ],
   imports: [
     BrowserModule,
