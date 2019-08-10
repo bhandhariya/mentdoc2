087f3a777079a2ddfa0db90bfc652b584fc43282
bhandhariya <sainiraja59@gmail.com>
2019-08-10 04:44:48 -0700

    index and security route creted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { SecutiryComponent } from './secutiry/secutiry.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:IndexComponent},
+  {path:'security',component:SecutiryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
