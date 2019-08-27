c5b5e3b7bdbfefea3c6ea92b1ca8edbff2533565
abhishek <abhishekbhati1993@gmail.com>
2019-08-27 14:36:00 +0530

    27 aug commit by abhishek

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { TestimonialComponent } from './shared/testimonial/testimonial.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { ChooseusComponent } from './shared/chooseus/chooseus.component';
+import { GalleryComponent } from './shared/gallery/gallery.component';
+import { AboutusComponent } from './shared/aboutus/aboutus.component';
+import { SliderComponent } from './shared/slider/slider.component';
+import { FeaturesComponent } from './shared/features/features.component';
+import { ModalsComponent } from './shared/modals/modals.component';
+import { AchievmentsComponent } from './shared/achievments/achievments.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    TestimonialComponent,
+    HeaderComponent,
+    ChooseusComponent,
+    GalleryComponent,
+    AboutusComponent,
+    SliderComponent,
+    FeaturesComponent,
+    ModalsComponent,
+    AchievmentsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
