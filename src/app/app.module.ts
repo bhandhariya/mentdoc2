bd1803f0409abc602711e310110f90fdda3013df
bhandhariya <sainiraja59@gmail.com>
2019-08-28 14:44:56 +0530

    header ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SecurityComponent } from './security/security.component';
+import { SHeaderComponent } from './security/shared/s-header/s-header.component';
+import { SBannerComponent } from './security/shared/s-banner/s-banner.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SecurityComponent,
+    SHeaderComponent,
+    SBannerComponent,
     
   ],
   imports: [
